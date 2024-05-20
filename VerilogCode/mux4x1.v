3c93a0188cb27e8d187ea3132eedffb5dae3120a
mostafa21314 <123311211+mostafa21314@users.noreply.github.com>
2024-05-20 05:00:36 +0300

    Update mux4x1.v to accomodate for coding guidlines

--- VerilogCode/mux4x1.v
+++ VerilogCode/mux4x1.v
@@ -1,15 +1,34 @@
+/*******************************************************************
+*
+* Module: mux4x1.v
+* Project: Alarm/Clock
+* Author: Malek Mahmoud, Malekrouk@aucegypt.edu
+* Author: Habiba Seif, habibaseif@aucegypt.edu
+* Description: This module implements a 4-to-1 multiplexer.
+*
+* Change history: 05/13/24 - Initial implementation
+*
+**********************************************************************/
+
 `timescale 1ns / 1ps
 
+module mux4x1 (
+    input wire [3:0] data0,
+    input wire [3:0] data1,
+    input wire [3:0] data2,
+    input wire [3:0] data3,
+    input wire [1:0] sel,
+    output reg [3:0] out
+);
 
-module mux4x1(input [3:0] data0,input [3:0] data1,input [3:0] data2,input [3:0] data3,input [1:0] sel,output reg [3:0] out);
-
-always @(*) begin
-    case(sel)
-        2'b00: out = data0;
-        2'b01: out = data1;
-        2'b10: out = data2;
-        2'b11: out = data3;
-    endcase
-end
+    always @(*) begin
+        case (sel)
+            2'b00: out = data0;
+            2'b01: out = data1;
+            2'b10: out = data2;
+            2'b11: out = data3;
+            default: out = 4'b0000; // Default case to handle unexpected sel values
+        endcase
+    end
 
 endmodule
