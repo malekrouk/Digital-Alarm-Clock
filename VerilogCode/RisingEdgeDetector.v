b2cf863c9c24c71ba0cd837c8f5560a57b3dbc4b
mostafa21314 <123311211+mostafa21314@users.noreply.github.com>
2024-05-20 04:59:57 +0300

    Update RisingEdgeDetector.v to accomodate for coding guidlines

--- VerilogCode/RisingEdgeDetector.v
+++ VerilogCode/RisingEdgeDetector.v
@@ -1,24 +1,50 @@
+/*******************************************************************
+*
+* Module: RisingEdgeDetector.v
+* Project: Alarm/Clock
+* Author: Malek Mahmoud, Malekrouk@aucegypt.edu
+* Author: Habiba Seif, habibaseif@aucegypt.edu
+
+* Description: This module detects rising edges on the input signal 'I'.
+*
+* Change history: 05/13/24 - Initial implementation
+*
+**********************************************************************/
+
 `timescale 1ns / 1ps
 
-module RisingEdgeDetector(input clk, rst, I, output O);
-reg [1:0] state, nextState;
-parameter [1:0] A=2'b00, B=2'b01, C=2'b10; 
+module RisingEdgeDetector (
+    input wire clk,
+    input wire rst,
+    input wire I,
+    output wire O
+);
+    reg [1:0] state, nextState;
+    parameter [1:0] A = 2'b00, B = 2'b01, C = 2'b10;
 
-always @ (I or state)
-case (state)
-A: if (I==0) nextState = A;
- else nextState = B;
-B: if (I==0) nextState = A;
- else nextState = C;
-C: if (I==0) nextState = A;
- else nextState = C;
-default: nextState = A;
-endcase
+    // State transition logic
+    always @(*) begin
+        case (state)
+            A: if (I == 0) nextState = A;
+               else nextState = B;
+            B: if (I == 0) nextState = A;
+               else nextState = C;
+            C: if (I == 0) nextState = A;
+               else nextState = C;
+            default: nextState = A;
+        endcase
+    end
 
-always @ (posedge clk or posedge rst) begin
-if(rst) state <= A;
-else state <= nextState;
-end
+    // State register
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            state <= A;
+        end else begin
+            state <= nextState;
+        end
+    end
 
-assign O = (state == B);
+    // Output logic
+    assign O = (state == B);
+
 endmodule
